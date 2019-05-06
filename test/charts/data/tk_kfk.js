7a972377bf2588a96f889d0c81e6c2f1b8c1f967
yangdengxian <yangdengxian@cnpc.com.cn>
2019-05-06 17:10:06 +0800

    修改测试代码

---
+++ test/charts/data/tk_kfk.js
@@ -0,0 +1,197 @@
+var tk_kfk = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [124.88, 46.71],
+            "type": "Point"
+        },
+        "properties": {
+            "data": [2415, 75799],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"],
+            "name": "大庆"
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [122.07, 41.21],
+            "type": "Point",
+            "colors": ["#1f77b4", "#ff7f0e"]
+        },
+        "properties": {
+            "name": "辽河",
+            "data": [1493, 23509],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [108.89, 34.49],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "长庆",
+            "data": [6892, 100553],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [86.21, 41.74],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "塔里木",
+            "data": [345, 2909],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [84.98, 45.61],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "新疆",
+            "data": [4342, 39803],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [104.09, 30.63],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "西南",
+            "data": [1365, 826],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [124.88, 45.18],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "吉林",
+            "data": [92, 35631],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [117.46, 38.79],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "大港",
+            "data": [302, 7097],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [94.56, 40.28],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "青海",
+            "data": [2872, 8600],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [116.05, 38.75],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "华北",
+            "data": [4316, 8358],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [93.42, 42.97],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "吐哈",
+            "data": [1212, 5297],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [118.12, 39.64],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "冀东",
+            "data": [77, 2789],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [98.43, 39.74],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "玉门",
+            "data": [349, 355],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [120.05, 30.33],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "浙江",
+            "data": [227, 373],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [110.38, 20.18],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "南方",
+            "data": [253, 257],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "coordinates": [116.35, 39.9],
+            "type": "Point"
+        },
+        "properties": {
+            "name": "煤层气",
+            "data": [409, 2897],
+            "key": ["探井", "开发井"],
+            "colors": ["#1f77b4", "#ff7f0e"]
+        }
+    }]
+}
